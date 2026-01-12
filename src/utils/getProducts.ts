a1bc577b6cd6a58a37cf45ab58f6f6ab443faa32
yliborio <yago.m.29@gmail.com>
2026-01-11 23:20:00 -0300

    feat: add cache invalidate rule for get products requests

--- src/utils/getProducts.ts
+++ src/utils/getProducts.ts
@@ -3,7 +3,9 @@ import { FakeAPIProduct } from "core/types/product";
 export const getProducts = async () => {
   let products: FakeAPIProduct[] = [];
   try {
-    const data = await fetch(`https://fakestoreapi.com/products`);
+    const data = await fetch(`https://fakestoreapi.com/products`, {
+      next: { revalidate: 60 },
+    });
     if (!data.ok) throw new Error(`Failed to fetch products: ${data.status}`);
     products = await data.json();
   } catch (err) {
